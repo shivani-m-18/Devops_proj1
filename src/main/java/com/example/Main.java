8977a776921d0155f770d8a6ad3e2109f9739b39
shivani-m-18 <shivanibiradar183@gmail.com>
2025-06-10 21:07:07 +0530

    res

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,13 +2,9 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello, World!");
     }
 }
-=======
 
-        System.out.println("Hello, World! hello");
-    }
-}
->>>>>>> 16492e988d858932acf797f16de95ccd10e94d04
+
