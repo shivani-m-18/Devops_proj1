16492e988d858932acf797f16de95ccd10e94d04
shivani-m-18 <shivanibiradar183@gmail.com>
2025-06-10 19:36:33 +0530

    ntng

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,13 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello, World! hello");
     }
 }
-// Testing Jenkins build trigger
-=======
-        System.out.println("Hello, World! how are you");
-    }
-}
->>>>>>> 7a7df051fb88874bf0fb32a79af5ded5a6b12d1e
