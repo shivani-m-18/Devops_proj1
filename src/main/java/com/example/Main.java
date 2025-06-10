a307565e8be2d1d5596cdf58c2c9f8192c6db35c
shivani-m-18 <shivanibiradar183@gmail.com>
2025-06-10 19:32:48 +0530

    this is some random commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! hello");
     }
 }
 // Testing Jenkins build trigger
